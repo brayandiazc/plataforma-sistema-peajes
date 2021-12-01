7b74f6daf83931664a3f51fc947b37bbdaa27789
WilmarAl <wilmaralvarez1998@gmail.com>
2021-11-30 19:38:31 -0500

    Estructuracion de carpetas

---
+++ src/components/ciudades.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Ciudades = () => {
+  return (
+    <section className="bg-light">
+      <div className="container">
+        <h1>View Ciudades</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Ciudades;
